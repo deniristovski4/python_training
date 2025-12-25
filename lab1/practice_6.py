bd776f9a78c825d4c0b1f41405df484831bd3a39
Deni Ristovski <deni.ristovski@avenga.com>
2025-12-25 13:03:45 +0100

    edited p6

--- lab1/practice_6.py
+++ lab1/practice_6.py
@@ -1,11 +1,7 @@
 # Write a program that from entered “Name Fathers_Name Surname” you will extract the Father’s name.
 
-Name=input("Внеси име: ")
-Fathers_Name=input("Внеси татково име: ")
-Surname=input("Внеси презиме: ")
+Name=input("Внеси име, татково име и презиме: ")
 
-full_name=Name+" "+Fathers_Name+" "+Surname
-
-result=full_name.split(" ")[1]
+result=Name.split(" ")[1]
 
 print(result)
